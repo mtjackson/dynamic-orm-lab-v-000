2795851f8051d39ca049c0a3c058ff291587f67d
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 22:49:00 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -11,6 +11,8 @@ class InteractiveRecord
     DB[:conn].results_as_hash = true
 
     sql = "pragma table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
   end
 
   def initialize
