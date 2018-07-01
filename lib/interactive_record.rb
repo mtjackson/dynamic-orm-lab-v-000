20acdbf86bb0b793c44ce96fc1b98e7e83cad987
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 22:56:00 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -43,7 +43,7 @@ class InteractiveRecord
   end
 
   def save
-    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})"
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES #{values_for_insert}"
   end
 
   def self.find_by_name
