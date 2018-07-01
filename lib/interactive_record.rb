44931a45507e71d0ac96f56065843cbaadcf3e28
Mary Jackson <jackson.marytherese@gmail.com>
2018-07-01 22:54:23 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -36,6 +36,10 @@ class InteractiveRecord
 
   def values_for_insert
     values = []
+    self.class.column_names.each do |column_name|
+      values << "'#{send(column_name)}'" unless send.column_name.nil?
+    end
+    values.join(", ")
   end
 
   def save
